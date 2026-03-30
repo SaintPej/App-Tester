c08b75dc21ced7fb631806a5d2da0ab77d8fe92b
SaintPej <jstabao14@gmail.com>
2026-03-30 14:35:40 +0800

    fix: add express.json middleware to parse request bodies

--- src/app.ts
+++ src/app.ts
@@ -48,7 +48,7 @@ const wrappedAuthHandler: express.RequestHandler = async (req, res, next) => {
 app.all("/api/auth/*", wrappedAuthHandler);
 app.all("/api/auth", wrappedAuthHandler);
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 app.use("/health", healthRouter);
 app.use("/me", meRouter);
